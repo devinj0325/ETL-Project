94d88dd7e16f21a0d9856c6b45461ff034e4bc6f
Devin <djwilson@vcu.edu>
2019-10-16 19:57:28 -0400

    updated schema file

--- schema/schema.sql
+++ schema/schema.sql
@@ -6,8 +6,15 @@ CREATE TABLE forcebycounty
     county VARCHAR,
     total_employees INT,
     officers INT,
-    civilians INT
+    civilians INT,
+    fips INT,
     CONSTRAINT forcebycounty_pkey PRIMARY KEY (id)
 )
 ;
+CREATE TABLE offensesbycounty
+(
+    id SERIAL,
+    county VARCHAR,
+    
 
+)
